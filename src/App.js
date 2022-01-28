374bd893dd36a5781cd5be709188271c887f15bb
Mukilarasi <mukilarasig@gmail.com>
2022-01-28 17:49:48 +0530

    UI material changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-
+import Button from '@mui/material/Button';
+// import Rating from '@material-ui/lab/Rating';
+// import TextField from '@mui/material/TextField';
+import TextField from '@mui/material/TextField';
+import Rating from '@mui/material/Rating';
+import Box from '@mui/material/Box';
+// import TextField from '@material-ui/core/TextField';
+//  import Typography from '@material-ui/core/Typography';
 
 export default function App() {
   let movies = [
     {
+     
       movie: "Jai Bhim",
       poster:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS_iVBAztnYSkxHkYwkLMX1mi1EDYr9D5Te4yHoMZmxQ5o6Qywotjtfbg-4KC8YyjGWuc&usqp=CAU",
@@ -12,6 +20,7 @@ export default function App() {
       summary: "A pregnant woman from a primitive tribal community, searches desperately for her husband, who is missing from police custody. A High Court advocate rises in support to find her husband and seek justice for them. "
     },
     {
+      
       movie: "Doctor",
       poster: "https://www.sify.com/uploads/Doctor_6_vkgob2afjjahj.jpeg",
       rating: " 8.5",
@@ -23,7 +32,7 @@ export default function App() {
          rating: "7.5",
       summary: "Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people."
     },
-    {
+    {  
       movie: "Teddy",
       poster:"https://m.media-amazon.com/images/M/MV5BODhlYTRhNGItZGJmMi00YjJmLWE2ZWYtNjBlNzQ2NjhjODc5XkEyXkFqcGdeQXVyOTk3NTc2MzE@._V1_.jpg",
       rating: "8.5",
@@ -36,13 +45,39 @@ export default function App() {
       summary: "John Durairaj, an alcoholic professor, gets enrolled to teach at a juvenile facility, unbeknownst to him. He soon clashes with a ruthless gangster, who uses the children as scapegoats for his crimes."
     }
   ];
-  return (
+  const [movie,setMovie]=useState("");
+   const [poster,setPoster]=useState("");
+   const [rating,setRating]=useState("");
+   const [summary,setSummary]=useState("");
+   const [movieList,setMovieList]=useState(movies);
+ return (
+  <div className="data">
+    <h1>Movie details</h1>
+    <div className="adddata">
+    <TextField value={movie}  label="movie name" onChange={(val)=>setMovie(val.target.value)}/> <br/>
+    <TextField value={poster}   label="poster"  onChange={(val)=>setPoster(val.target.value)}/> <br/>
+     <TextField value={rating}   label="ratings"  onChange={(val)=>setRating(val.target.value)}/> <br/> 
+        <TextField value={summary}  label="summary" onChange={(val)=>setSummary(val.target.value)}/> <br/>
+      
+        <Button variant="contained"onClick={()=>{
+        const newMovie ={
+          movie: movie,
+          poster:poster,
+          rating:rating,
+          summary:summary,
+        };
+         setMovieList([...movieList,newMovie])
+      }}>Add Movie
+        </Button>
+        </div>
+      
     <div className="container">
-      {movies.map(({ movie, poster, rating, summary }) => (
+      {movieList.map(({ movie, poster, rating, summary }) => (
         <Sample name={movie} url={poster} star={rating} sum={summary} />
-        
       ))}
+      {/* <GetData/> */}
       {/* <Counter/> */}
+    </div>
     </div>
   );
 }
@@ -52,38 +87,40 @@ function Counter(){
   const[like, setLike]=useState(10);
   const[dislike,setDislike]=useState(10);
   return(
-    <div className="btn"><button onClick={()=>setLike(like + 1)}>👍{like}</button>
-    <button onClick={()=>setDislike(dislike+1)}>👎{dislike}</button>
-    {/* <h3><button onClick={()=>setShow(!show)}className="sum">summary:</button></h3> */}
-         
-    
-    {/* <h3>{like}</h3>
+    <div className="btn"><Button variant="contained" onClick={()=>setLike(like + 1)}>👍{like}</Button>
+    {/* <button onClick={()=>setDislike(dislike+1)}>👎{dislike}</button> */}
+    <Button variant="contained" onClick={()=>setDislike(dislike+1)}>👎{dislike}</Button>
+       {/* <h3>{like}</h3>
     <h3>{like}</h3>
     <h3>{like}</h3>
     <h3>{like}</h3> */}
     </div>
   )
-}
-// function Clicked({
+} 
 
-// })
 function Sample({ name, url, star, sum }) {
+  // conditional styling
   const styles ={color: star>=8.5? "teal":"red"};
   const[show,setShow]=useState(false);
+  // const toggler =()=>{
+  //   show ? setShow(false):setShow(true);
+  // }
+
   return (
     <div>
         <div className="card">
         <img src={url} alt={name} />
         <div className="movie-details">
           <h3><span className="mo">movie:</span>{name}</h3>
-          <h3 style={styles}><span className="rat">rating:</span> {star}</h3>
+      <Rating name="customized-10" defaultValue={2} max={10} />
           <Counter/>
-          <button onClick={()=>setShow(!show)}>Summary</button>
-         {show?<h3>{sum}</h3>:null}
-          
+           <Button variant="contained" onClick={()=>setShow(!show)}>Summary</Button>
+           {
+           show?<h3>{sum}</h3>:false
+            }
+
           </div>
         </div>
-      </div>
+      </div>     
   );
 }
-// export default App;
